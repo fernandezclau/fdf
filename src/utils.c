18672321eb39d47ddec4d4f5fda838601bf3ebad
fernandezclaudia <cfernandezg@bcds.net>
2024-04-12 14:59:01 +0200

    utils

--- src/utils.c
+++ src/utils.c
@@ -19,35 +19,17 @@ void	clean_line(char *line)
 /*
 ** DESC: The 'process_line' process a line pouring it into the matrix.
 */
-void	process_line(char *file_name, t_map *map)
+void	process_line(char *file_name, char  *z_matrix)
 {
-
-}
-/*
-** DESC: Scaletes the coordinates.
-*/
-void    scale_coords(t_coords *coords_a, t_coords *coords_b, int scale)
-{
-    /*(*coords_a).x*/
-    coords_a->x *= scale;
-    coords_a->y *= scale;
-    coords_b->x *= scale;
-    coords_b->y *= scale;
-}
-
-/*
-** DESC: Convert coords to isometric proyection.
-*/
-void to_isometric(t_coords *coords_a, t_coords *coords_b, char **matrix)
-{
-    int a_z;
-    int b_z;
-
-    a_z = matrix[coords_a.x][coords_a.y];
-    b_z = matrix[coords_b.x][coords_b.y];
-    coords_a->x = (coords_a->x  - coords_a->y) * cos(0.5);
-    coords_a->y = (coords_a->x + coords_a->y ) * sin(0.5) - a_z;
-
-    coords_b->x = (coords_b->x  - coords_a->y) * cos(0.5);
-    coords_b->y = (coords_a->x + coords_a->y ) * sin(0.5) - b_z;
+	char **items;
+	int	i;
+	
+	i = 0;
+	items = ft_split(line, ' ');
+	while(items[i])
+	{
+		*z_matrix[i] = ft_atoi(items[i]);
+		free(items[i]);
+		i++;
+	}
 }
