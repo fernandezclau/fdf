2c973fb7aa91b8ad293922e25f477db3a8b92360
fernandezclaudia <cfernandezg@bcds.net>
2024-04-12 12:26:42 +0200

    adding utils

--- src/utils.c
+++ src/utils.c
@@ -12,16 +12,28 @@ int	get_max(int num1, int num2)
 /*
 ** DESC: Scaletes the coordinates.
 */
-void    scale_coords(t_coords coords_a, t_coords coords_b)
+void    scale_coords(t_coords *coords_a, t_coords *coords_b, int scale)
 {
-    
+    /*(*coords_a).x*/
+    coords_a->x *= scale;
+    coords_a->y *= scale;
+    coords_b->x *= scale;
+    coords_b->y *= scale;
 }
 
 /*
-angulo es x
+** DESC: Convert coords to isometric proyection.
 */
-void isometric()
+void to_isometric(t_coords *coords_a, t_coords *coords_b, char **matrix)
 {
-    x = (x  - y) * cos(angle);
-    y = (x + y ) * sin(angle) - z;
+    int a_z;
+    int b_z;
+
+    a_z = matrix[coords_a.x][coords_a.y];
+    b_z = matrix[coords_b.x][coords_b.y];
+    coords_a->x = (coords_a->x  - coords_a->y) * cos(0.5);
+    coords_a->y = (coords_a->x + coords_a->y ) * sin(0.5) - a_z;
+
+    coords_b->x = (coords_b->x  - coords_a->y) * cos(0.5);
+    coords_b->y = (coords_a->x + coords_a->y ) * sin(0.5) - b_z;
 }
