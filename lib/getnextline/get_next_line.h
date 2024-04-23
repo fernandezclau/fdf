4919cc237daac6474e1e2e52b217b71e1ddb1c86
Clau_42 <fernandezclau1@gmail.com>
2024-04-23 16:45:31 +0200

    fix header getnextline

--- lib/getnextline/get_next_line.h
+++ lib/getnextline/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: claferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 16:38:14 by claferna          #+#    #+#             */
-/*   Updated: 2024/04/16 20:29:30 by claferna         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:44:33 by claferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,8 @@
 # include <fcntl.h>		//OPEN, CLOSE LIB
 
 /*MAIN FUNCTION*/
-<<<<<<< Updated upstream
-char		*get_next_line(int fd);
-int			ft_read(t_list_g **list, int fd);
-char		*ft_get_line(t_list_g *list);
-void		ft_clean_list(t_list_g **list);
-/*UTILS FUNCTIONS*/
-void		ft_extract_line_lst(t_list_g *list, char *line);
-void		ft_erase_elements(t_list_g **list, t_list_g *clean_node, \
-		char *next_line);
-int			ft_find_line(t_list_g *lst);
-void		ft_lstadd_line(t_list_g **list, char *buffer);
-t_list_g	*ft_lstlast_item(t_list_g *list);
-int			ft_get_len_line(t_list_g *list);
-=======
 int		get_next_line(int fd, char **line);
 int		get_line(int fd, char **line);
 char		*set_malloc(char **line, char *buffer);
 int		add_line(char **line, char *buffer);
->>>>>>> Stashed changes
 #endif
